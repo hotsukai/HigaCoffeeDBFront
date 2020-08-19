faa7784ff16db7495fa6751610d42a2510825a0f
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-08-19 16:09:12 +0900

    add login

---
+++ pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="field">
+      <p class="control has-icons-left has-icons-right">
+        <input class="input" type="email" placeholder="メールアドレス" v-model="mail" />
+      </p>
+    </div>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="password" placeholder="パスワード" v-model="pass" />
+      </p>
+    </div>
+    <div class="field">
+      <p class="control">
+        <button class="button is-success" @click="login">ログイン</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import { auth } from "../plugins/firebase";
+@Component({
+  layout: "default",
+  components: {},
+})
+export default class LoginPage extends Vue {
+  mail: string = "";
+  pass: string = "";
+
+   login() {
+    auth.signInWithEmailAndPassword(this.mail, this.pass)
+      .then(user => this.$router.push('/'))
+      .catch(e => alert(e.message))
+  }
+}
+</script>
