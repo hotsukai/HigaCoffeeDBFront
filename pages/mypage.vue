0ae2facf2f84ac08cee0c03e036bb3e083957177
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-08-19 23:54:47 +0900

    add mypage

---
+++ pages/mypage.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>お名前:{{user_name}}</p>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import firebase from "../plugins/firebase";
+import { User } from "firebase";
+@Component({})
+export default class MypagePage extends Vue {
+  private name: string | null = "初期の名前";
+
+  public get user_name(): string | null {
+    return this.name;
+  }
+  created() {
+    const user: User | null = firebase.auth().currentUser;
+    if (user != null) {
+      this.name = user.displayName;
+      console.log(user.displayName);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
