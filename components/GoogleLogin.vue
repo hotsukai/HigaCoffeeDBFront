8dfbe6ffc4afd7a46ceaff841aa31f2daf6bb019
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-08-21 18:30:11 +0900

    update enable to have reviews id

--- components/GoogleLogin.vue
+++ components/GoogleLogin.vue
@@ -1,27 +1,51 @@
 <template>
-    <a class="googleLogin" @click="signInWithGoogleRedirect">グーグルでログイン </a>
+  <a class="googleLogin" @click="signInWithGooglePopup">グーグルでログイン</a>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import firebase from "../plugins/firebase"
+import { Component, Vue } from "vue-property-decorator";
+import firebase from "../plugins/firebase";
 
-const googleProvider = new firebase.auth.GoogleAuthProvider()
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+var db = firebase.firestore();
 @Component
 export default class GoogleLogin extends Vue {
-  public signInWithGoogleRedirect () {
-    firebase.auth().signInWithRedirect(googleProvider);
+  sendUser(user: any): void {
+    db.collection("users").doc(user.uid)
+      .set({
+        uid: user.uid,
+        name: user.displayName,
+        reviews:[]
+        // user_id: firebase.auth().currentUser.uid
+      })
+      .then(function () {
+        console.log("add user data ");
+      })
+      .catch(function (error: any) {
+        console.error("Error adding document: ", error);
+      });
+  }
+  
+  signInWithGooglePopup() {
+    firebase
+      .auth()
+      .signInWithPopup(googleProvider)
+      .then( (result) => {
+        var user = result.user;
+        console.log(user);
+        this.sendUser(user);
+      });
   }
 }
 </script>
 
 
 <style scoped>
-  a.googleLogin {
-    display: block;
-    cursor: pointer;
-    width: 191px;
-    height: 46px;
-    margin: 0 auto;
-  }
+a.googleLogin {
+  display: block;
+  cursor: pointer;
+  width: 191px;
+  height: 46px;
+  margin: 0 auto;
+}
 </style>
