8bf230d54af5ae11ab555eb1759afae56279eb0f
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-08-21 18:12:30 +0900

    some fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="container">
-    <h1>トップページ</h1>
+    <h1 class="title">トップページ</h1>
     <div v-if="isLogin">
       <p>ログイン中</p>
       <button @click="logout">ログアウト</button>
+    </div>
+    <div v-else>
+      <p>ログインしていません</p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import firebase from "@/plugins/firebase";
+@Component
+export default class indexPage  extends Vue{
+ isLogin: boolean = false;
+  async mounted() {
+    await firebase
+      .auth()
+      .onAuthStateChanged((user) => (this.isLogin = user ? true : false));
+  }}
 </script>
 
 <style>
