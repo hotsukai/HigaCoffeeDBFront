8bd30e0c0b06b85db468d939cf8228af3faaec34
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-08-30 17:35:33 +0900

    update secretword

--- pages/login.vue
+++ pages/login.vue
@@ -2,28 +2,55 @@
   <div>
     <h1>ログイン</h1>
     <div class="field">
-      <p class="control">
-        <!-- TODO: 合言葉入力フォーム -->
-        <GoogleLogin/>
-      </p>
+      <div class="control">
+        <input v-model="inputSecretWord" placeholder="合言葉は？？" type="password" />
+        <div v-if="isSecretWordCorrect">
+          <GoogleLogin />
+        </div>
+        <div v-else>
+          <p>合言葉が間違っています</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { Vue, Component } from "vue-property-decorator";
-import firebase from "../plugins/firebase";
+import firebase from "@/plugins/firebase";
+const db = firebase.firestore();
 
-@Component
 
-export default class LoginPage extends Vue {
-  mounted() {
+export default {
+  data() {return{
+    inputSecretWord:"",
+    secretWord:""}
+  },
+
+ async mounted() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.$router.push("/");
       } else {
       }
     });
+
+    await db
+      .collection("secretWord")
+      .doc("2020")
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          this.secretWord = doc.data().secretWord;
+        }
+      });
+  },
+
+  computed: {
+    isSecretWordCorrect() {
+
+      return this.secretWord===this.inputSecretWord
+    },
   }
 }
 
