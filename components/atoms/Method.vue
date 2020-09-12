2af4552daa1d2dfdd8037d4f6860341d70261555
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-12 18:15:22 +0900

    add ExtractionMethod template

---
+++ components/atoms/Method.vue
@@ -0,0 +1,42 @@
+<template>
+  <span>{{extractionMethod}}</span>
+</template>
+
+<script>
+import firebase from "@/plugins/firebase";
+const db = firebase.firestore();
+export default {
+  props: ["em"],
+  data() {
+    return {
+      unsafeExtractionMethod: "",
+    };
+  },
+  async mounted() {
+    await db
+      .collection("methods")
+      .doc(String(this.em))
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          this.unsafeExtractionMethod = doc.data().name;
+        }
+      });
+  },
+  computed: {
+    extractionMethod() {
+      if (
+        this.unsafeExtractionMethod < 0 ||
+        this.unsafeExtractionMethod == null
+      ) {
+        return "設定されていません";
+      } else {
+        return this.unsafeExtractionMethod;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
