95f1cd94cc2d2999568c945ad538c124f28016b5
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-12 11:38:01 +0900

    update support mobile view of navbar

--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -1,20 +1,28 @@
 <template>
   <div>
     <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-menu">
-        <div class="navbar-start">
-          <nuxt-link to="/" class="navbar-item">トップへ</nuxt-link>
-          <a class="navbar-item">データを見る</a>
-          <!--TODO: データページ作成-->
+      <div class="navbar-brand">
+        <nuxt-link to="/" class="navbar-item">HiGaCoffeeDBProject</nuxt-link>
+        <div
+          class="navbar-burger burger"
+          data-target="navbarMenu"
+          @click="toggleMenu()"
+          :class="{'is-active':isMenuActive}"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </div>
+      </div>
+      <div class="navbar-menu" id="navbarMenu" :class="{'is-active':isMenuActive}">
         <div class="navbar-end">
-          <div class="navbar-item">
-            <nuxt-link to="/mypage" class="navbar-item">マイページ</nuxt-link>
-            <nuxt-link to="/reviews/create" class="navbar-item">レビューを書く</nuxt-link>
-            <nuxt-link to="/coffees/create" class="navbar-item">コーヒーを淹れる</nuxt-link>
-            <div class="buttons">
-              <LoginButton />
-            </div>
+          <nuxt-link to="/" class="navbar-item">データを見る</nuxt-link>
+          <!--TODO: データページ作成-->
+          <nuxt-link to="/mypage" class="navbar-item">マイページ</nuxt-link>
+          <nuxt-link to="/reviews/create" class="navbar-item">レビューを書く</nuxt-link>
+          <nuxt-link to="/coffees/create" class="navbar-item">コーヒーを淹れる</nuxt-link>
+          <div class="buttons">
+            <LoginButton />
           </div>
         </div>
       </div>
@@ -22,17 +30,26 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
-@Component({
-  layout: "default",
-  components: {},
-})
-export default class HeaderComponents extends Vue {}
+<script>
+export default {
+  data: () => {
+    return { isMenuActive: false };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuActive = !this.isMenuActive;
+    },
+  },
+  watch: {
+    '$route' () {
+      this.isMenuActive = false
+    }
+  }
+};
 </script>
 
 <style scoped>
-.navbar-menu {
-  background-color: darkgoldenrod;
+.navbar {
+  background-color: #34bb81;
 }
 </style>
