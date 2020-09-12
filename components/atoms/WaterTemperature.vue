a6c44c0b6baa30401b2d1ce0fceeafac83000431
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-12 17:53:13 +0900

    add waterTemperature component

---
+++ components/atoms/WaterTemperature.vue
@@ -0,0 +1,22 @@
+<template>
+  <span>{{wtData}}</span>
+</template>
+
+<script>
+export default {
+  props: ["wt"],
+  computed: {
+    wtData() {
+        console.debug(this.wt)
+      if (this.wt < 0 || this.wt == null) {
+        return "設定されていません";
+      } else {
+        return wt;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
