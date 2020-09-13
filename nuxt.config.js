efa0a569dbaabe6de7a83b073a408ac72760ddc8
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-13 16:47:26 +0900

    for netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,12 +5,15 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
-  mode: 'universal',
+  mode: 'spa',
+  //mode:'universal
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'server',
+  // target: 'server',
+  target: 'static',
+
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
