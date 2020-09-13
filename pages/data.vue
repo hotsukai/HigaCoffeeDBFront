69c1184b4ae90a0b722cd1fd33246aac67e5118d
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-13 14:58:04 +0900

    update data

--- pages/data.vue
+++ pages/data.vue
@@ -1,13 +1,35 @@
 <template>
-  
+  <div>
+    <section>
+      <p class="title">セクション1どの豆??</p>
+      <button class="button">概要(団体)</button>
+      <button class="button">濃さ(団体)</button>
+      <button class="button">概要(個人)</button>
+      <button class="button">濃さ(個人)</button>
+    </section>
+    <hr />
+    <section>
+      <p class="title">セクション2:どうやって淹れる??</p>
+      <div class="select">
+        <select>
+          <option v-for="bean in beans" v-bind:key="bean.id">{{bean.name}}</option>
+        </select>
+        <button class="button">団体</button>
+        <button class="button">個人</button>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      beans: this.$beanNames,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
