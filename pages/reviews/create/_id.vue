dab715502da2e0c524865d5d22a2061c8301ee4b
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-25 10:44:43 +0900

    fix get coffeeData

--- pages/reviews/create/_id.vue
+++ pages/reviews/create/_id.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
     <p>ID:{{coffeeId}}のコーヒーについて</p>
-    <ReviewForm :coffeeId="coffeeId"></ReviewForm>
+    <ReviewForm :coffee="coffeeData"></ReviewForm>
   </div>
 </template>
 
 <script>
+import firebase from "@/plugins/firebase";
+const db = firebase.firestore();
+
 export default {
   data() {
     return {
-      coffeeId: "",
+      coffeeData: null,
+      coffeeId:null
     };
   },
+
   created() {
-    this.coffeeId = this.$route.params.id;
+    let coffeeId = this.$route.params.id;
+    db.collection("coffees")
+      .doc(coffeeId)
+      .get()
+      .then((doc) => {
+        this.coffeeData = doc.data();
+        this.coffeeId=doc.id
+      });
   },
 };
 </script>
