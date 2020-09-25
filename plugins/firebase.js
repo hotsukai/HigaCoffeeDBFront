540ff73bc7e36731f7528205d929d831d9fcc5d7
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-25 20:47:15 +0900

    fix for deploy

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -5,14 +5,15 @@ import 'firebase/firestore'
 // SDKを使用するためのConfig情報
 // TODO: dotenv
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+
+  apiKey:"AIzaSyDYlHpVlwkHdbjVGLBuSkmBCRnPM5Gwnfk",
+  authDomain: "higadbapp.firebaseapp.com",
+  databaseURL: "https://higadbapp.firebaseio.com",
+  projectId: "higadbapp",
+  storageBucket:"higadbapp.appspot.com",
+  messagingSenderId: "166806901337",
+  appId:  "higadbapp1:166806901337:web:bc94f0e1f87025d4ed1748",
+  measurementId:  "G-KFXGGC7ZM1"
 };
 // Appの初期化
 if (!firebase.apps.length) {
