20a48246143644d055ac773aa6344ddff714e172
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-10-03 18:32:23 +0900

    [update] change font-weight bold if selected

--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -7,19 +7,43 @@
           class="navbar-burger burger"
           data-target="navbarMenu"
           @click="toggleMenu()"
-          :class="{'is-active':isMenuActive}"
+          :class="{ 'is-active': isMenuActive }"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </div>
       </div>
-      <div class="navbar-menu" id="navbarMenu" :class="{'is-active':isMenuActive}">
+      <div
+        class="navbar-menu"
+        id="navbarMenu"
+        :class="{ 'is-active': isMenuActive }"
+      >
         <div class="navbar-end">
-          <nuxt-link to="/data" class="navbar-item">データを見る</nuxt-link>
-          <nuxt-link to="/mypage" class="navbar-item">マイページ</nuxt-link>
-          <nuxt-link to="/reviews/create" class="navbar-item">レビューを書く</nuxt-link>
-          <nuxt-link to="/coffees/create" class="navbar-item">コーヒーを淹れる</nuxt-link>
+          <nuxt-link
+            to="/data"
+            class="navbar-item"
+            :class="{ isSelected: isDataSelected }"
+            >データを見る</nuxt-link
+          >
+          <nuxt-link
+            to="/mypage"
+            class="navbar-item"
+            :class="{ isSelected: isMypageSelected }"
+            >マイページ</nuxt-link
+          >
+          <nuxt-link
+            to="/reviews/create"
+            class="navbar-item"
+            :class="{ isSelected: isCreateReviewSelected }"
+            >レビューを書く</nuxt-link
+          >
+          <nuxt-link
+            to="/coffees/create"
+            class="navbar-item"
+            :class="{ isSelected: isCreateCoffeeSelected }"
+            >コーヒーを淹れる</nuxt-link
+          >
           <div class="buttons">
             <LoginButton />
           </div>
@@ -32,25 +56,72 @@
 <script>
 export default {
   data: () => {
-    return { isMenuActive: false };
+    return {
+      isMenuActive: false,
+      isDataSelected: false,
+      isMypageSelected: false,
+      isCreateReviewSelected: false,
+      isCreateCoffeeSelected: false,
+    };
   },
-  
+
+  created() {
+    this.changeSelectedPage();
+  },
+
   methods: {
     toggleMenu() {
       this.isMenuActive = !this.isMenuActive;
     },
+
+    changeSelectedPage() {
+      this.isDataSelected = false;
+      this.isMypageSelected = false;
+      this.isCreateReviewSelected = false;
+      this.isCreateCoffeeSelected = false;
+      switch (this.$route.name) {
+        case "data":
+          this.isDataSelected = true;
+          break;
+        case "mypage":
+          this.isMypageSelected = true;
+          break;
+        case "reviews-create":
+          this.isCreateReviewSelected = true;
+          break;
+        case "coffeescreate":
+          this.isCreateCoffeeSelected = true;
+          break;
+        default:
+          console.debug("selected route", this.$route.name);
+          break;
+      }
+    },
   },
 
   watch: {
-    '$route' () {
-      this.isMenuActive = false
-    }
-  }
+    $route() {
+      this.isMenuActive = false;
+      this.changeSelectedPage();
+    },
+  },
 };
 </script>
 
 <style scoped>
 .navbar {
-  background-color: #34bb81;
+  background-color: #303033;
+}
+
+.navbar-menu {
+  background-color: #303033;
+}
+
+.navbar-item {
+  color: #e1dfdb;
+}
+
+.isSelected {
+  font-weight: bold;
 }
 </style>
