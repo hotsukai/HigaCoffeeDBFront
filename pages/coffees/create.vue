80c02971d642a2ccaf5be5ed9aae4acb4ba87ee2
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-12-05 17:34:28 +0900

    [update] フォームの雛形を作成

--- pages/coffees/create.vue
+++ pages/coffees/create.vue
@@ -1,14 +1,69 @@
-// TODO:コーヒーを新しく淹れる
 <template>
-  <div class>
-    <h1 class="title">コーヒーを淹れる</h1>
-    <p>Comming soon...!!!</p>
+  <div>
+    <form>
+      <div>
+        <label class="label">豆の種類</label>
+      </div>
+      <div>
+        <label class="label">抽出法</label>
+      </div>
+      <div>
+        <label class="label">抽出時間</label>
+        <!-- TODO:フレンチプレスのみ -->
+      </div>
+      <div>
+        <label class="label">粉の量</label>
+      </div>
+      <div>
+        <label class="label">お湯の量</label>
+      </div>
+      <div>
+        <label class="label">お湯の温度</label>
+      </div>
+      <div>
+        <label class="label">飲む人</label>
+      </div>
+      <div>
+        <label class="label">メモ</label>
+        <p class="help">
+          既定のレシピ通りに出来なかった場合はその旨を記してください（例：お湯を入れすぎた、抽出時間長すぎた）
+        </p>
+        <input class="input" type="text" v-model="feeling" />
+      </div>
+      <p v-show="!isValid" class="is-danger">入力に不備があります</p>
+      <button
+        @click="sendCoffee"
+        v-bind:disabled="!isValid"
+        type="button"
+        class="button is-primary"
+      >
+        送信!!
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+import firebase from "@/plugins/firebase";
+var db = firebase.firestore();
+
+export default {
+  props: ["coffeeData"],
+
+  data() {
+    return {};
+  },
+
+  computed: {
+    isValid() {
+      return true;
+    }
+  },
+
+  methods: {
+    sendCoffee() {}
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
