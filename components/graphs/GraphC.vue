4a13c2f0dda73ce5d0b3e8deae3af9005932d586
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-12-22 17:35:15 +0900

    [fix] グラフCのリファクタリング

--- components/graphs/GraphC.vue
+++ components/graphs/GraphC.vue
@@ -1,6 +1,6 @@
 <script>
 import { Bubble } from "vue-chartjs";
-
+import Chart from "chart.js";
 export default {
   extends: Bubble,
   props: ["getPositionDataPromise"],
@@ -8,56 +8,8 @@ export default {
   data() {
     return {
       data: {
-        labels: this.$beanNames,
-        datasets: [
-          {
-            label: "ブラジル中煎り",
-            data: [{ x: 0, y: 0, r: 7 }],
-            backgroundColor: "rgba(100, 10, 10,0.5)"
-          },
-          {
-            label: "ブラジル深煎り",
-            data: [{ x: 0, y: 0, r: 7 }],
-
-            backgroundColor: "rgba(100, 10, 10,1)"
-          },
-          {
-            label: "コロンビア中煎り",
-            data: [{ x: 0, y: 0, r: 7 }],
-
-            backgroundColor: "rgba(10, 100, 10,0.5)"
-          },
-          {
-            label: "コロンビア深煎り",
-            data: [{ x: 0, y: 0, r: 7 }],
-
-            backgroundColor: "rgba(10, 100, 10,1)"
-          },
-          {
-            label: "タンザニア中煎り",
-            data: [{ x: 0, y: 0, r: 7 }],
-
-            backgroundColor: "rgba(10, 10, 100,0.5)"
-          },
-          {
-            label: "タンザニア深煎り",
-            data: [{ x: 0, y: 0, r: 7 }],
-
-            backgroundColor: "rgba(10, 10, 100,1)"
-          },
-          {
-            label: "インドネシア中煎り",
-            data: [{ x: 0, y: 0, r: 7 }],
-
-            backgroundColor: "rgba(10, 50, 50,0.5)"
-          },
-          {
-            label: "インドネシア深煎り",
-            data: [{ x: 0, y: 0, r: 7 }],
-
-            backgroundColor: "rgba(10, 50, 50,1)"
-          }
-        ]
+        labels: [],
+        datasets: []
       },
       options: {
         responsive: true,
@@ -104,23 +56,38 @@ export default {
   },
 
   async mounted() {
-    await this.getPositionDataPromise.then(receivedData => {
-      if (receivedData) {
-        console.debug("d1 : ");
+    const backgroundColorsArray = [
+      "rgba(100, 10, 10,0.5)",
+      "rgba(100, 10, 10,1)",
+      "rgba(10, 100, 10,0.5)",
+      "rgba(10, 100, 10,1)",
+      "rgba(10, 10, 100,0.5)",
+      "rgba(10, 10, 100,1)",
+      "rgba(10, 50, 50,0.5)",
+      "rgba(10, 50, 50,1)"
+    ];
+    await this.getPositionDataPromise.then(receivedDataObject => {
+      if (receivedDataObject) {
         for (
           let beanIterator = 1;
-          beanIterator <= Object.keys(receivedData).length;
+          beanIterator <= Object.keys(receivedDataObject).length;
           beanIterator++
         ) {
-          console.debug(
-            "data : ",
-            JSON.stringify(receivedData[beanIterator].avgBitterness)
-          );
-          this.data.datasets[beanIterator - 1].data[0].x =
-            receivedData[beanIterator].avgSituation || 0;
-          this.data.datasets[beanIterator - 1].data[0].y =
-            receivedData[beanIterator].avgBitterness || 0;
+          this.data.labels.push(receivedDataObject[beanIterator].beanName);
+          const beanData = {
+            label: receivedDataObject[beanIterator].beanName,
+            backgroundColor: backgroundColorsArray[beanIterator - 1],
+            data: [
+              {
+                x: receivedDataObject[beanIterator].avgSituation || 0,
+                y: receivedDataObject[beanIterator].avgBitterness || 0,
+                r: 7
+              }
+            ]
+          };
+          this.data.datasets.push(beanData);
         }
+
         this.renderChart(this.data, this.options);
       } else {
         console.warn("データがありません");
