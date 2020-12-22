7e8954e2ab7815516b0f7f48cd1055ab12301bad
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-12-22 13:03:04 +0900

    [fix] usersの雛形を作成

---
+++ pages/users/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>
+      準備中のコンテンツです
+    </p>
+    <nuxt-link to="/">トップへ</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
