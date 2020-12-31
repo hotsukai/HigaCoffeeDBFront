ef2bbaf41e27be54d04f5492883346c3f96bec15
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-12-31 12:19:52 +0900

    add: linterを導入

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+
+  module.exports = {
+    root: true,
+    parser: "vue-eslint-parser",
+    parserOptions: {
+      parser: "babel-eslint",
+      sourceType: "module",
+    },
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    extends: [
+      // 上から順にルールが適用（上書き）されていく
+      // 個別のルールは`.prettierrc.js`に書く
+      "eslint:recommended",
+      "plugin:vue/recommended",
+      "prettier",
+      "prettier/vue",
+    ],
+    plugins: ["vue"],
+    rules: {},
+  };
