dc3544d6d8ada30815ea985e9b9b449cbfa48eec
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2021-03-20 16:51:39 +0900

    fix 余白多めスタイル

--- layouts/toppage.vue
+++ layouts/toppage.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- TODO:メタ情報 -->
-  <div>
+  <div style="padding-bottom:3rem;">
     <GlobalMenu />
     <Nuxt />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,99 +1,88 @@
 <template>
   <div>
     <div id="bg1" class="parallax-bg overlay">
-      <h1 class="title is-size-1-mobile">Higa = 彼我</h1>
-    </div>
-    <div id="bg2" class="parallax-bg">
       <div class="section">
+        <h1 class="title is-size-mobile is-size-2-mobile">Higa = 彼我</h1>
         <div class="columns">
           <div class="column">
-            <p>
-              データでコーヒーはもっと楽しめる。<br />
-              みんなのデータを使ったらもっともっと楽しめる。
+            <p class="poem">
+              データでコーヒーはもっと楽しめる。
+              <br />みんなのデータを使ったらもっともっと楽しめる。
             </p>
 
-            <p>
-              Higa Coffee データベースは <br />
-              "いつどんな目的で飲んだのか",<br />
-              "どんな印象を持ったのか"<br />
+            <p class="poem">
+              Higa Coffee データベースは
+              "いつどんな目的で飲んだのか",
+              "どんな印象を持ったのか"
               を記録し、データをもとに一人ひとりにぴったりなコーヒーを見つけるお手伝いをします。
             </p>
 
-            <p>あなたのぴったりを見つけよう</p>
-            <button
-              type="button"
-              class="button"
-              @click="$router.push('/signup')"
-            >
-              サインアップ
-            </button>
+            <p class="poem">あなたのぴったりを見つけよう</p>
+            <button type="button" class="button" @click="$router.push('/signup')">サインアップ</button>
           </div>
         </div>
       </div>
     </div>
-    <div id="bg3" class="parallax-bg">
+    <div class="container">
+      <div class="section what-is-this">
+        <div class="columns">
+          <div class="column">
+            <p class="sub-title data">みる</p>
+            <p>
+              どんなコーヒーがあなたにぴったり?
+              <br />"みる"ではみんなのコーヒーのデータをグラフで見ることができます。あなたはどんなコーヒーをどうやって淹れることにしますか?
+            </p>
+          </div>
+
+          <div class="column">
+            <p class="sub-title reviews">よむ</p>
+            <p>
+              みんなはコーヒーどう思ってるの?
+              <br />"よむ"ではみんなのコーヒーのレビューを読むことができます。みんなのレビューからあなたの好みの傾向を探りましょう。
+            </p>
+          </div>
+
+          <div class="column">
+            <p class="sub-title reviews-create">かく</p>
+            <p>
+              のんだコーヒーの記録をつけましょう!
+              <br />"かく"ではコーヒーの味わいを記録することができます。その記録があなたや誰かのぴったり探しの助けになるかもしれません。
+            </p>
+          </div>
+
+          <div class="column">
+            <p class="sub-title coffees-create">いれる</p>
+            <p>なんとなくいれるじゃもったいない!<br>※現在、HigaCoffeeメンバーのみの提供になっております。</p>
+          </div>
+        </div>
+      </div>
       <div class="section">
         <div class="columns">
           <div class="column">
-            <h2>What's Higa Coffee?</h2>
-            <p>
-              コミュニケーションを大切にしながらコーヒーを楽しみたい。<br />
+            <img src="/images/coffee3.jpg" />
+          </div>
+          <div class="column">
+            <h2 class="is-size-3 poem">Higa Coffee とは??</h2>
+            <p class="poem">
+              コミュニケーションを大切にしながらコーヒーを楽しみたい。
               そんな思いを込めて、Higa Coffeeと名付けました。
             </p>
-            <p>
-              「どんなコーヒーが好きか」というのは極めて個人的な問題ですが、<br />
-              私たちはこの問題を仲間と一緒に考えることを大切にしています。
-            </p>
-            <p>
-              〈あなた とわたし〉の違い、つまり〈彼我〉の差を手掛かりにして<br />
-              それぞれが ”自分好みのコーヒー” を探しているのです。
+
+            <p class="poem">
+              〈あなたとわたし〉
+              <br />の違い、つまり
+              <br />〈彼我〉
+              <br />の差を手掛かりにして
+              それぞれが "自分好みのコーヒー" を探しているのです。
             </p>
 
-            <p>
-              2019年に生まれ、2020年になんとか自立した小さな団体です。少しでもご関心をお持ちの方はお気軽にご連絡ください。あなたの参加を心よりお待ちしております。
-            </p>
-            <a href="https://sites.google.com/view/higa-coffee/top"
-              ><p>HiGa Coffeeをもっと知る>></p></a
-            >
-
-            <p>あなたのぴったりを見つけよう</p>
-            <button
-              type="button"
-              class="button"
-              @click="$router.push('/signup')"
-            >
-              サインアップ
-            </button>
+            <p
+              class="poem"
+            >2019年に生まれ、2020年になんとか自立した小さな団体です。少しでもご関心をお持ちの方はお気軽にご連絡ください。あなたの参加を心よりお待ちしております。</p>
+            <a href="https://sites.google.com/view/higa-coffee/top">
+              <p>HiGa Coffeeをもっと知る>></p>
+            </a>
           </div>
-        </div>
-      </div>
-    </div>
-    <div class="section what-is-this">
-      <div class="columns">
-        <div class="column">
-          <p class="sub-title data">みる</p>
-          <p>
-            どんなコーヒーがあなたにぴったり?<br />"みる"ではみんなのコーヒーのデータをグラフで見ることができます。あなたはどんなコーヒーをどうやって淹れることにしますか?
-          </p>
-        </div>
-
-        <div class="column">
-          <p class="sub-title reviews">よむ</p>
-          <p>
-            みんなはコーヒーどう思ってるの?<br />"よむ"ではみんなのコーヒーのレビューを読むことができます。みんなのレビューからあなたの好みの傾向を探りましょう。
-          </p>
-        </div>
-
-        <div class="column">
-          <p class="sub-title reviews-create">かく</p>
-          <p>
-            あなたのぴったりの記録をつけましょう!<br />"かく"ではコーヒーの味わいを記録することができます。
-          </p>
-        </div>
-
-        <div class="column">
-          <p class="sub-title coffees-create">いれる</p>
-          <p>なんとなくいれるじゃもったいない!</p>
         </div>
       </div>
     </div>
@@ -108,59 +97,52 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-#bg1 {
-  background-image: url("/images/coffee3.jpg");
-  min-height: 100vh;
-  &::after {
-    content: "";
-    background-color: rgba(0, 0, 0, 0.2);
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    height: 100%;
-  }
-  .title {
-    font-size: 5rem;
-    color: $white;
-    position: relative;
-    z-index: 1;
-  }
-}
-#bg2 {
-  background-image: url("/images/coffee2.jpg");
-  min-height: 60vh;
-  position: relative;
-  &::after {
-    content: "";
-    background-color: rgba(0, 0, 0, 0.5);
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    height: 100%;
-  }
-  p {
-    margin-bottom: 2rem;
-    position: relative;
-    z-index: 1;
-  }
-  button {
-    margin-bottom: 2rem;
-    position: relative;
-    z-index: 1;
-  }
-}
-#bg3 {
-  background-image: url("/images/coffee4.jpg");
-  p {
-    margin-bottom: 2rem;
-  }
-}
-
 .parallax-bg {
+  &#bg1 {
+    background-image: url("/images/coffee1.jpg");
+    min-height: 100vh;
+    &::after {
+      content: "";
+      background-color: rgba(0, 0, 0, 0.2);
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      height: 100%;
+    }
+    .title {
+      font-size: 5rem;
+      color: $white;
+      position: relative;
+      z-index: 1;
+    }
+  }
+  &#bg2 {
+    background-image: url("/images/coffee2.jpg");
+    min-height: 60vh;
+    position: relative;
+    &::after {
+      content: "";
+      background-color: rgba(0, 0, 0, 0.5);
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      height: 100%;
+    }
+    p {
+      margin-bottom: 2rem;
+      position: relative;
+      z-index: 1;
+    }
+    button {
+      margin-bottom: 2rem;
+      position: relative;
+      z-index: 1;
+    }
+  }
   background-attachment: fixed;
   background-position: center;
   background-size: cover;
@@ -172,7 +154,9 @@ export default Vue.extend({
   justify-content: center;
   padding: 5%;
 }
-
+.section {
+  padding: 5rem 2rem 0 2rem;
+}
 .what-is-this {
   .sub-title {
     font-size: 1.3rem;
@@ -207,5 +191,8 @@ export default Vue.extend({
     font-weight: 900;
     content: "\f253";
   }
+}
+.poem {
+  margin-bottom: 1rem;
 }
 </style>
